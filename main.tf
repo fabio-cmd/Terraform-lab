e53b0d7633ac3492dafee0104c6389047395429e
Fabio Diniz <fdiniz704@gmail.com>
2025-06-05 18:23:43 -0300

    feat:labs-terraform-created

---
+++ main.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.55.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "vm-lab" {
+  image    = "ubuntu-22-04-x64"
+  name     = var.droplet_name
+  region   = var.droplet_region
+  size     = var.droplet_size
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+data "digitalocean_ssh_key" "ssh_key" {
+  name = var.ssh_key_name
+}
+
+resource "digitalocean_firewall" "firewall_ocean" {
+  name = "only-22-80-and-443"
+
+  droplet_ids = [digitalocean_droplet.vm-lab.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "53"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "22"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "80"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "443"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+}
+
+variable "do_token" {
+
+}
+
+variable "droplet_name" {
+  default = "vm-lab"
+
+}
+
+variable "droplet_region" {
+  default = "nyc1"
+
+}
+
+variable "droplet_size" {
+  default = "s-1vcpu-2gb"
+
+}
+
+variable "ssh_key_name" {
+  default = "digital-ocean"
+
+}
+
+output "droplet_ip" {
+  value = digitalocean_droplet.vm-lab.ipv4_address
+}
